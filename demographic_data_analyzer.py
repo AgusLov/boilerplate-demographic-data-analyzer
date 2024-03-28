e021ee98713d5d4423d756b74ee4855db8fc665e
Agustin Lovecchio <agustinlovecchio359@gmail.com>
2024-03-27 22:43:16 -0300

    comentando en español

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -1,40 +1,41 @@
+# Importa la librería pandas
+
 import pandas as pd
 
 
 def calculate_demographic_data(print_data=True):
-    # Read data from file
+    # Leer los datos desde un archivo CSV
     df = pd.read_csv('adult.data.csv')
-
-    # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
+    # Número de personas de cada raza
     race_count = df['race'].value_counts()
 
-    # What is the average age of men?
+    # Edad promedio de los hombres
     average_age_men = round(df[df['sex'] == 'Male']['age'].mean(), 1)
     
-    # What is the percentage of people who have a Bachelor's degree?
+    # Porcentaje de personas con título de Bachiller
     percentage_bachelors = round(len(df[df['education']== 'Bachelors'])/len(df['education'])*100, 1)
     
 
-    # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
-    # What percentage of people without advanced education make more than 50K?
+    # Porcentaje de personas con educación avanzada (Bachiller, Maestría o Doctorado) que ganan más de $50K
+    # Porcentaje de personas sin educación avanzada que ganan más de $50K
     
-    # with and without `Bachelors`, `Masters`, or `Doctorate`
+    # Con y sin Bachiller, Maestría u Doctorado
     higher_education = df[df['education'].isin(['Bachelors', 'Masters', 'Doctorate'])]
     lower_education = df[~df['education'].isin(['Bachelors', 'Masters', 'Doctorate'])]
 
-    # percentage with salary >50K
+    # Porcentaje con salario >50K
     higher_education_rich = round(len(higher_education[higher_education['salary'] == '>50K']) / len(higher_education) * 100, 1)
     lower_education_rich = round(len(lower_education[lower_education['salary'] == '>50K']) / len(lower_education) * 100, 1)
 
-    # What is the minimum number of hours a person works per week (hours-per-week feature)?
+    # Cantidad mínima de horas que una persona trabaja a la semana
     min_work_hours = df['hours-per-week'].min()
 
-    # What percentage of the people who work the minimum number of hours per week have a salary of >50K?
+    # Porcentaje de personas que trabajan el mínimo de horas y ganan >50K
     num_min_workers = df[df['hours-per-week']==min_work_hours]
 
     rich_percentage =  round(len(num_min_workers[num_min_workers['salary'] == '>50K']) / len(num_min_workers) * 100, 1)
 
-    # What country has the highest percentage of people that earn >50K?
+    # País con mayor porcentaje de personas que ganan >50K
     count_country = df['native-country'].value_counts()
     rich_country_count = df[df['salary'] == '>50K']['native-country'].value_counts()
     
@@ -43,7 +44,7 @@ def calculate_demographic_data(print_data=True):
     
     highest_earning_country_percentage = round((rich_country_count/count_country*100).max(),1)
 
-    # Identify the most popular occupation for those who earn >50K in India.
+    # Ocupaciones más populares en la India para personas que ganan >50K
     rich_indians = df[(df['native-country']=='India')  & (df['salary']=='>50K')]
     
     top_IN_occupation = rich_indians['occupation'].value_counts().idxmax()
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-# This entrypoint file to be used in development. Start by reading README.md
-import demographic_data_analyzer
+
+# Este es un archivo "entrypoint" que se utilizará en el desarrollo. Comienza
+# leyendo README.mdimport demographic_data_analyzer
 from unittest import main
 
-# Test your function by calling it here
+# Llama a tu función aquí para probarla
 demographic_data_analyzer.calculate_demographic_data()
 
-# Run unit tests automatically
+# Ejecuta las pruebas unitarias automáticamente
 main(module='test_module', exit=False)
